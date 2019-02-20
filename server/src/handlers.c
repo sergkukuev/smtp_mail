f510ab362405914642f6fec105b5988190ea68e8
Segey Kukuev <serg.kukuev@yandex.ru>
2019-02-20 15:40:10 +0300

    Stub for handlers smtp command

---
+++ server/src/handlers.c
@@ -0,0 +1,8 @@
+#include "handlers.h"
+
+// main smtp handler (can parse all command)
+void main_handler(struct cs_data_t* cs)
+{
+    // stub
+    return;
+}
---
+++ server/src/handlers.h
@@ -0,0 +1,9 @@
+#ifndef __HANDLERS_H__
+#define __HANDLERS_H__
+
+#include "common.h"
+
+// main smtp handler (can parse all command)
+void main_handler(struct cs_data_t* cs);
+
+#endif
