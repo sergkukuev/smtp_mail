4d4dbd59949d27f9a5bd4408fda8cf9aa1a4d66a
sergkukuev <serg.kukuev@yandex.ru>
2019-02-22 20:46:45 +0300

    #3 Fix logic parser select()

--- server/src/process.c
+++ server/src/process.c
@@ -1,46 +1,95 @@
 #include "process.h"
 #include "sockets.h"
+#include "smtp.h"
 
-#include <sys/types.h>
-#include <unistd.h>
 #include <signal.h>
+#include <sys/select.h>
+#include <mqueue.h>
 
 // process initialize
-struct process_t* init_process(pid_t pid, pid_t log_pid, struct ss_node_t* ss) 
+struct process_t* init_process(int pid, int log_pid, int* fd) 
 {
     struct process_t* proc = (struct process_t*) malloc(sizeof *proc);
     proc->pid = pid;
     proc->worked = true;
-    proc->max_fd = -1;
-    char tmp[50];
-
-    // log init
-    sprintf(tmp, "/process%d", log_pid);
-    proc->lg = log_pid;
-    proc->lg_name = tmp;
-
-    // mqueue initialize
-    sprintf(tmp, "/process%d", getpid());
-    proc->mq_name = tmp;
-
-    struct mq_attr attr;
-    attr.mq_flags = attr.mq_curmsgs = 0;
-    attr.mq_maxmsg = 10;
-    attr.mq_msgsize = BUFFER_SIZE;
-
-    proc->mq = malloc(sizeof *proc->mq);
-    *(proc->mq) = mq_open(proc->mq_name, O_CREAT | O_RDONLY | O_NONBLOCK, 0644, &attr);    // 0644: write, read, read
-    if (*(proc->mq) == -1) {
-        perror("mq_open() failed");
-        free(proc->mq);
-    } else if (*(proc->mq) > proc->max_fd)  // check fd message queue
-        proc->max_fd = *(proc->mq);
-
+    proc->l_fd = *fd;
+    proc->max_fd = (fd == NULL) ? 0 : *fd;
     // lists of sockets
-    proc->ss_list = NULL;
-    proc->ls_list = init_client_sockets(ss, &proc->max_fd);
-
+    proc->ws_list = NULL;
+    proc->rs_list = NULL;
+    // log init
+    proc->lg = log_pid;
+    // message queue for logger
+    if (pid == log_pid) {
+        char name[BUFFER_SIZE];
+        sprintf(name, "/process%d", getpid());
+        struct mq_attr attr;
+        attr.mq_flags = attr.mq_curmsgs = 0;
+        attr.mq_maxmsg = 10;
+        attr.mq_msgsize = BUFFER_SIZE;
+        proc->mq = malloc(sizeof *proc->mq);
+        *(proc->mq) = mq_open(name, O_CREAT | O_RDONLY | O_NONBLOCK, 0644, &attr);    // 0644: write, read, read
+        if (*(proc->mq) == -1) {
+            perror("mq_open() failed");
+            free(proc->mq);
+        } else if (*(proc->mq) > proc->max_fd)  // check fd message queue
+            proc->max_fd = *(proc->mq);
+    }
     return proc;
+}
+
+bool check_listen_socket(int fd, struct cs_node_t** head, fd_set* readfds, int* max_fd)
+{
+    if (FD_ISSET(fd, readfds)) {
+        struct cs_data_t* data = accept_client_socket(fd);
+        if (data) {
+            *max_fd = data->fd > *max_fd ? data->fd : *max_fd;   // check max fd
+            struct cs_node_t* node = malloc(sizeof(*node)); // insert at head ws_list
+            node->cs = data;
+            node->next = *head;
+            *head = node;
+            return true;
+        }
+    }
+    return false;
+}
+
+// parser select()
+void parse_select(struct process_t* proc)
+{
+	struct timeval tv;	// timeout for select
+	tv.tv_sec = SELECT_TIMEOUT;
+	tv.tv_usec = 0;
+	// call select: can change timeout
+	int ndesc = select(proc->max_fd + 1, &(proc->readfds), &(proc->writefds), NULL, &tv);
+	switch(ndesc) {
+		// error
+		case -1:
+			perror("select() failed");
+			break;
+		// no events - close sockets
+		case 0:
+			printf("Server(%d): Timeout\n", getpid());
+			for (struct cs_node_t* i = proc->ws_list; i != NULL; i = i->next)
+				if (!FD_ISSET(i->cs->fd, &(proc->readfds)))
+					i->cs->state = SOCKET_STATE_CLOSED;
+			break;
+		// sockets ready - need checks
+		default:
+            check_listen_socket(proc->l_fd, &proc->ws_list, &proc->readfds, &proc->max_fd);
+            // handle sockets from ws_list
+            for (struct cs_node_t* tmp = proc->ws_list; tmp != NULL; tmp = tmp->next) {
+                if (FD_ISSET(tmp->cs->fd, &proc->readfds)) {
+                    tmp->cs->flw = false;
+                    main_handle(tmp->cs, proc->lg);
+                }
+                if (FD_ISSET(tmp->cs->fd, &proc->writefds)) {
+                    tmp->cs->flw = true;
+                    main_handle(tmp->cs, proc->lg);
+                }
+            }
+            break;
+	}
 }
 
 // runner
@@ -50,28 +99,25 @@ void run_process(struct process_t* proc)
         // clear fd
         FD_ZERO(&(proc->readfds));
         FD_ZERO(&(proc->writefds));
+        FD_SET(proc->l_fd, &proc->readfds);
 
         // delete all closed sockets
-        proc->ss_list = delete_sockets_by_state(proc->ss_list, SOCKET_STATE_CLOSED);
+        close_client_sockets_by_state(&(proc->ws_list), SOCKET_STATE_CLOSED);
 
         // log
         char bf[BUFFER_SIZE];
-        sprintf(bf, "ls_list = %p is_null = %d", proc->ls_list, (proc->ls_list == NULL));
-        mq_log(proc->lg, bf);
-        sprintf(bf, "ss_list = %p is_null = %d", proc->ss_list, (proc->ss_list == NULL));
-        mq_log(proc->lg, bf);
+        sprintf(bf, "writters = %p is_null = %d", proc->ws_list, (proc->ws_list == NULL));
+        //mq_log(proc->lg, bf);
+        sprintf(bf, "readers = %p is_null = %d", proc->rs_list, (proc->rs_list == NULL));
+        //mq_log(proc->lg, bf);
 
-        // set all sockets to ss_list
-        for (struct cs_node_t* i = proc->ls_list; i != NULL; i = i->next)
-            FD_SET(i->cs.fd, &(proc->readfds));
-        // set exists client socket
-        for (struct cs_node_t* i = proc->ss_list; i != NULL; i = i->next) {
-            FD_SET(i->cs.fd, &(proc->readfds));
-            FD_SET(i->cs.fd, &(proc->writefds));
+        // sets all sockets in array
+        for (struct cs_node_t* i = proc->rs_list; i != NULL; i = i->next)
+            FD_SET(i->cs->fd, &(proc->readfds));
+        for (struct cs_node_t* i = proc->ws_list; i != NULL; i = i->next) {
+            FD_SET(i->cs->fd, &(proc->readfds));
+            FD_SET(i->cs->fd, &(proc->writefds));
         }
-        // set message queue
-        if (proc->mq != NULL)
-            FD_SET(*(proc->mq), &(proc->readfds));
         // call select() and proc->worked change inside
         parse_select(proc);
     }
@@ -80,49 +126,66 @@ void run_process(struct process_t* proc)
 // cleanup
 void free_process(struct process_t* proc)
 {
-    delete_client_sockets(&proc->ls_list);
-    delete_client_sockets(&proc->ss_list);
-    mq_close(*(proc->mq));
-    mq_unlink(proc->mq_name);
-    free(proc->mq);
-
+    close_client_sockets_by_state(&proc->rs_list, SOCKET_NOSTATE);
+    close_client_sockets_by_state(&proc->ws_list, SOCKET_NOSTATE);
+    
+    if (proc->mq != NULL) {
+        char name[BUFFER_SIZE];
+        sprintf(name, "/process%d", *(proc->mq));
+        mq_close(*(proc->mq));
+        mq_unlink(name);
+        free(proc->mq);
+    }
     free(proc);
     printf("Server(%d): process killed\n", getpid());
     kill(getpid(), SIGTERM);
 }
 
-// process creating
-// return process id or -1
-pid_t create_process(struct ss_node_t* fd_socket, pid_t log_pid)
+void body_process(int* fd_socket, int log_pid)
 {
-    pid_t pid = fork();
+    struct process_t* proc = init_process(getpid(), log_pid, fd_socket);
+    // open mq logger
+    char name[BUFFER_SIZE];
+    sprintf(name, "/process%d", proc->lg);
+    mqd_t lg = mq_open(name, O_WRONLY); 
+    if (lg > 0) proc->lg = lg;
+    // write in log
+    char bf[BUFFER_SIZE];
+    sprintf(bf, "child process forked with pid(%d), ppid(%d)", getpid(), getppid());
+    //mq_log(proc->lg, bf);
+    
+    run_process(proc);
+    free_process(proc);
+}
+
+// create and run process
+// returns pid
+int create_process(int* listen_fd, int log_pid, void (*body)(int*, int))
+{
+    int pid = fork();
     switch (pid) {
         // process not created
         case -1: 
             printf("Server(%d): fork() failed\n", getpid()); 
             break;
         // process-child
-        case 0: {
-            struct process_t* proc = init_process(getpid(), log_pid, fd_socket);
-            // open mq logger
-            mqd_t lg = mq_open(proc->lg_name, O_WRONLY); 
-            if (lg > 0)
-                proc->lg = lg;
-            // write in log
-            char bf[BUFFER_SIZE];
-            sprintf(bf, "child process forked with pid(%d)", getpid());
-            mq_log(proc->lg, bf);
-            sprintf(bf, "his parent pid(%d)", getppid());
-            mq_log(proc->lg, bf);
-            
-            run_process(proc);
-            free_process(proc);
+        case 0: 
+            (*body)(listen_fd, log_pid);
             break;
-        }
         // process-parent
         default:
             printf("Server(%d): create proccess(%d)\n", getpid(), pid);
             break;
     }
     return pid;
+}
+
+// create processes
+// returns pid array
+int* create_processes(int fd, int lg, int* nproc)
+{
+    int* pids = malloc(sizeof(*pids) * *nproc);
+    for (int i = 0; i < *nproc; i++)
+        *(pids + i) = create_process(&fd, lg, body_process);
+    return pids;
 }
--- server/src/process.h
+++ server/src/process.h
@@ -3,11 +3,38 @@
 
 #include "common.h"
 
-// create and run process
-// returns process structure
-pid_t create_process(struct ss_node_t* socket_fd, pid_t log_pid);
+// struct of process data
+struct process_t {
+    int pid;
+    bool worked;
+    int l_fd;       // listen fd
+    int max_fd;     // used select
 
-// process initialization (for logger)
-struct process_t* init_process(pid_t pid, pid_t log_pid, struct ss_node_t* ss);
+    // uses as fd
+    int* mq;  // used only logger
+    int lg;
+
+    // sets
+    fd_set writefds;
+    fd_set readfds;
+    // lists of clients sockets
+    struct cs_node_t* ws_list;
+    struct cs_node_t* rs_list;
+};
+
+// create and run process
+// returns pid
+int create_process(int* listen_fd, int log_pid, void (*body)(int*, int));
+
+// create and run processes
+// returns pids array
+int* create_processes(int s_fd, int log_pid, int* nproc);
+
+// process initialization
+// return structure
+struct process_t* init_process(int pid, int log_pid, int* fd);
+
+// process free
+void free_process(struct process_t* proc);
 
 #endif
