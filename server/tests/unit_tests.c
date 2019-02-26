b173cf7fc467eaa72e5e03eff5df3184b1ef975b
Segey Kukuev <serg.kukuev@yandex.ru>
2019-02-26 09:34:52 +0300

    #6 Change printf

--- server/tests/unit_tests.c
+++ server/tests/unit_tests.c
@@ -40,7 +40,7 @@ int main(void) {
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
-    printf("===============================UNIT_TESTS_START===============================\n");
+    printf("================================LOG================================\n");
     CU_basic_show_failures(CU_get_failure_list());
-    printf("================================UNIT_TESTS_END================================\n");
+    printf("================================END================================\n");
 }
