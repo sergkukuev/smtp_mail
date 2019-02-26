fc6e7c946a94bc69ed1a5460282f5a0c56d2d4c7
Segey Kukuev <serg.kukuev@yandex.ru>
2019-02-26 10:27:32 +0300

    #6 Fix defines

--- server/tests/unit/test_main.c
+++ server/tests/unit/test_main.c
@@ -1,9 +1,18 @@
 #include "test_main.h"
+#include "utils.h"
+
+// know that there is better not to do
+#include "test_handlers.c"
+
+/*TEST(full_session_correct)
+{
+}*/
 
 int add_mytests(CU_pSuite suite)
 {
     if (suite) {
         // TODO: add tests here
+        ADD_SUITE_TEST(suite, handle_helo_correct)
     }
     return CU_get_error();
 }
--- server/tests/unit/test_main.h
+++ server/tests/unit/test_main.h
@@ -5,14 +5,6 @@
 #include <stdlib.h>
 #include <CUnit/Basic.h>
 
-#define TEST(name) \
-    static void test_##name()
-
-#define ADD_SUITE_TEST(suite, name) \
-    if ((NULL = CU_add_suite(suite, #name, (CU_TestFunc)test_##name))) { \
-        CU_cleanup_registry(); \
-    }
-
 int add_mytests(CU_pSuite suite);
 
 #endif
